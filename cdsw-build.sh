be641e176ee5cb2d9dcf70383694a576b09bb33a
zBrainiac <marcel.daeppen@gmail.com>
2020-04-06 14:22:49 +0200

    un-comment

--- cdsw-build.sh
+++ cdsw-build.sh
@@ -9,19 +9,19 @@
 
 #NLTK, a natural language toolkit for Python. A useful package for any natural language processing.
 
-#pip3 install -U nltk
+pip3 install -U nltk
 
 #stop_words, a Python package containing stop words.
 
-#pip3 install stop-words
+pip3 install stop-words
 
 #gensim, a topic modeling package containing our LDA model.
 
-#pip3 install gensim
+pip3 install gensim
 
 #pyLDAvis Python library for interactive topic model visualization. Port of the R LDAvis package.
 
-#pip3 install pyLDAvis
+pip3 install pyLDAvis
 
 
 #######################################
